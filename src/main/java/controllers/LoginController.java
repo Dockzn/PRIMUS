9986a1b94a1c696a2b19b839267706fc1829aad5
arthdantas <arthdantas05@gmail.com>
2024-09-06 19:13:12 -0300

    Criação da Controller de Login no Sistema

---
+++ src/main/java/controllers/LoginController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button botaoLogin;
+
+    @FXML
+    private TextField campoMatricula;
+
+    @FXML
+    private TextField campoSenha;
+
+    @FXML
+    void realizarLogin(ActionEvent event) {
+        //LoginDAO
+    }
+
+}
