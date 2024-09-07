f83913deea39bb8758d846b4d5ed22cd0bf2cd8d
DaviS <davilegalbr@gmail.com>
2024-09-07 17:08:54 -0300

    view e controller exibicaoNoticia

---
+++ primus/src/main/java/br/com/primus/primus/ExibicaoNoticiasController.java
@@ -0,0 +1,53 @@
+package br.com.primus.primus;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class ExibicaoNoticiasController {
+
+    @FXML
+    private Button BotaoCriarNoticia;
+
+    @FXML
+    private Button botaoAcessarNoticia;
+
+    @FXML
+    private Button botaoAtualizarNoticia;
+
+    @FXML
+    private Button botaoDeletarNoticia;
+
+    @FXML
+    private Button botaoFecharNoticia;
+
+    @FXML
+    private TableView<?> tabelaAcessarNoticia;
+
+    @FXML
+    void acessarNoticia(ActionEvent event) {
+
+    }
+
+    @FXML
+    void atualizarNoticia(ActionEvent event) {
+
+    }
+
+    @FXML
+    void criarNoticia(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deletarNoticia(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fecharNoticia(ActionEvent event) {
+
+    }
+
+}
