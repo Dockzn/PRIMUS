8764b8d680895117c59346e64b4511a62727c67d
arthdantas <arthdantas05@gmail.com>
2024-09-07 10:07:41 -0300

    Adicao da classe App para executar as telas e adicao de bibliotecas na classe LoginController

---
+++ src/main/java/controllers/CadastroController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CadastroController {
+
+    @FXML
+    private Button botaoCadastrar;
+
+    @FXML
+    private TextField campoMatricula;
+
+    @FXML
+    private TextField campoSenha;
+
+    @FXML
+    void realizarCadastro(ActionEvent event) {
+        
+    }
+
+}
--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,11 +1,17 @@
 package controllers;
 
 import models.repository.LoginDAO;
+
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController {
 
