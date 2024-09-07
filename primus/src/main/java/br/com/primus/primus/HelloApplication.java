20eaf116bfefb422a592140733ec46c1387fb535
arthdantas <arthdantas05@gmail.com>
2024-09-07 16:20:48 -0300

    Implementacao da view de cadastro

--- primus/src/main/java/br/com/primus/primus/HelloApplication.java
+++ primus/src/main/java/br/com/primus/primus/HelloApplication.java
@@ -12,9 +12,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         // Corrija o caminho para o arquivo FXML. 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("cadastro-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Tela de Login");
+        stage.setTitle("Tela de Cadastro");
         stage.setScene(scene);
         stage.show();
     }
