124182d8322931a19ce88be72a25379645bb55dc
arthdantas <arthdantas05@gmail.com>
2024-09-07 17:01:06 -0300

    Implementacao inicial da view de Exibicao de Atividades

---
+++ primus/src/main/java/br/com/primus/primus/ExibicaoAtividadesController.java
@@ -0,0 +1,49 @@
+package br.com.primus.primus;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ExibicaoAtividadesController {
+
+    @FXML
+    private Button acessarAtividade;
+
+    @FXML
+    private Button atualizarAtividade;
+
+    @FXML
+    private Button criarAtiviade;
+
+    @FXML
+    private Button deletarAtividade;
+
+    @FXML
+    private TableColumn<?, ?> idColumn;
+
+    @FXML
+    private TableView<?> tabelaAtividades;
+
+    @FXML
+    void acessarAtividade(ActionEvent event) {
+        
+    }
+
+    @FXML
+    void atualizarAtividade(ActionEvent event) {
+
+    }
+
+    @FXML
+    void criarAtividade(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deletarAtividade(ActionEvent event) {
+
+    }
+
+}
---
+++ primus/src/main/java/br/com/primus/primus/models/repository/ExibicaoAtividadesDAO.java
@@ -0,0 +1,12 @@
+
+package br.com.primus.primus.models.repository;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExibicaoAtividadesDAO {
+    private static Map<String, String> Atividades = new HashMap<>();
+
+    public void inserirAtividade(){
+
+    }
+}
