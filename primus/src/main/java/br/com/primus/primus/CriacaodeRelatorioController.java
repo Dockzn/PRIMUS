e3ba66ca9bdf7bb35a3890e1c32f5c3b4cdac111
Diego Amorim - Dockzn <diego91077@gmail.com>
2024-09-07 16:26:16 -0300

    Adição view criacao de relatorio, controller e escopo do dao

---
+++ primus/src/main/java/br/com/primus/primus/CriacaodeRelatorioController.java
@@ -0,0 +1,64 @@
+package br.com.primus.primus;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+
+public class CriacaodeRelatorioController {
+
+    @FXML
+    private Button botaoCancelarRelatorio;
+
+    @FXML
+    private RadioButton botaoOpcaoBia;
+
+    @FXML
+    private RadioButton botaoOpcaoPrex;
+
+    @FXML
+    private Button botaoSalvarRelatorio;
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    private ImageView iconeRelatorioSalvo;
+
+    @FXML
+    private Label labelRelatorioSalvo;
+
+    @FXML
+    private ImageView logoPrimus;
+
+    @FXML
+    private TextArea texteAreaEscreverRelatorio;
+
+    @FXML
+    void cancelarRelatorio(ActionEvent event) {
+
+    }
+
+    @FXML
+    void escolhaBia(ActionEvent event) {
+
+    }
+
+    @FXML
+    void escolhaPrex(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salvarRelatorio(ActionEvent event) {
+
+    }
+
+    @FXML
+    void voltarMenuDoProjeto(ActionEvent event) {
+
+    }
+
+}
--- primus/src/main/java/br/com/primus/primus/models/repository/RelatorioDAO.java
+++ primus/src/main/java/br/com/primus/primus/models/repository/RelatorioDAO.java
@@ -1,41 +1,55 @@
-// package models.repository;
-
-// import java.sql.SQLException;
-
-// import models.entity.Relatorio;
+// package br.com.primus.primus.models.repository;
+// import java.util.HashMap;
+// import java.util.Map;
 
 // public class RelatorioDAO {
+//     private static Map<String, String> relatorios = new HashMap<>();
 
 //     /**
-//      * Envia Relatório a ser enviado seja para o BIA ou para o PREX
+//      * Salva Relatório a ser enviado seja para o BIA ou para o PREX.
+//      * Adiciona o relatório no HashMap.
 //      */
-//     public void enviarRelatorio(Relatorio relatorio) throws SQLException {
-
+//     public void salvarRelatorio(Relatorio relatorio) {
+//         // Simula salvar o relatório usando um identificador único como chave
+//         String idRelatorio = relatorio.getId(); // Assumindo que o relatório tenha um ID único
+//         relatorios.put(idRelatorio, relatorio.getConteudo()); // 'Conteudo' é o texto ou dados do relatório
+//         System.out.println("Relatório salvo: " + idRelatorio);
 //     }
 
 //     /**
-//      * Atualiza Relatório a ser enviado seja para o BIA ou para o PREX
+//      * Cancela (remove) o Relatório a ser enviado seja para o BIA ou para o PREX.
+//      * Remove um relatório do HashMap com base no ID.
 //      */
-
-//     public void atualizarRelatorio(Relatorio relatorio) throws SQLException {
-
+//     public void cancelarRelatorio(Relatorio relatorio) {
+//         String idRelatorio = relatorio.getId(); // Assumindo que o relatório tenha um ID único
+//         if (relatorios.containsKey(idRelatorio)) {
+//             relatorios.remove(idRelatorio);
+//             System.out.println("Relatório removido: " + idRelatorio);
+//         } else {
+//             System.out.println("Relatório não encontrado para cancelamento.");
+//         }
 //     }
 
-
 //     /**
-//      * Cancela Relatório a ser enviado seja para o BIA ou para o PREX
+//      * Lê o Relatório a ser enviado seja para o BIA ou para o PREX.
+//      * Retorna o conteúdo de um relatório existente no HashMap.
 //      */
-
-//     public void cancelarRelatorio(Relatorio relatorio) throws SQLException {
-        
+//     public String lerRelatorio(Relatorio relatorio) {
+//         String idRelatorio = relatorio.getId(); // Assumindo que o relatório tenha um ID único
+//         if (relatorios.containsKey(idRelatorio)) {
+//             return relatorios.get(idRelatorio); // Retorna o conteúdo do relatório
+//         } else {
+//             System.out.println("Relatório não encontrado.");
+//             return null;
+//         }
 //     }
 
-
 //     /**
-//      * Ler Relatório a ser enviado seja para o BIA ou para o PREX
+//      * Voltar para o Menu do Projeto (apenas um exemplo de operação).
+//      * Aqui você poderia simplesmente limpar o HashMap, se necessário.
 //      */
-
-//     public void lerRelatorio(Relatorio relatorio) throws SQLException {
-        
+//     public void voltarParaMenuDoProjeto() {
+//         relatorios.clear(); // Exclui todos os relatórios
+//         System.out.println("Voltando para o menu. Todos os relatórios foram limpos.");
 //     }
 // }
