b74571a5b245c3722b143a27465470e7ac76acaa
vascsx <vasczn@gmail.com>
2024-09-07 16:32:48 -0300

    tela de menu

--- primus/src/main/java/br/com/primus/primus/HelloApplication.java
+++ primus/src/main/java/br/com/primus/primus/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         // Corrija o caminho para o arquivo FXML. 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("menu-projeto.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Tela de Login");
         stage.setScene(scene);
---
+++ primus/src/main/java/br/com/primus/primus/MenuController.java
@@ -0,0 +1,66 @@
+package br.com.primus.primus;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView; // Atualize para ImageView
+
+public class MenuController {
+
+    @FXML
+    private Button botaoAtividades;
+
+    @FXML
+    private Button botaoGeral;
+
+    @FXML
+    private MenuButton botaoMais;
+
+    @FXML
+    private Button botaoNoticias;
+
+    @FXML
+    private ImageView iconeProjeto; // Atualize para ImageView
+
+    @FXML
+    private Label labelNomeProjeto;
+
+    @FXML
+    private TextArea textAreUltimasNoticias;
+
+    @FXML
+    private TextField textFieldAtividade1;
+
+    @FXML
+    private TextField textFieldAtividade2;
+
+    @FXML
+    private TextField textFieldAtividade3;
+
+ @FXML
+ void IrParaTelaCriarAtividade(ActionEvent event) {
+ }
+ @FXML
+ void irParaTelaCriarRelatorio(ActionEvent event) {
+ }
+ @FXML
+ void irParaTelaDeAtividades(ActionEvent event) {
+ }
+ @FXML
+ void irParaTelaDeNoticias(ActionEvent event) {
+ }
+ @FXML
+ void irParaTelaDocumentacao(ActionEvent event) {
+ }
+ @FXML
+ void irParaTelaMaisInfoProjeto(ActionEvent event) {
+ }
+ @FXML
+ void irParaTelaPrincipalDoProjeto(ActionEvent event) {
+ }
+
+}
