31bf5f2046a661336f84145b6770bfcb7e9dbb24
Diego Amorim - Dockzn <diego91077@gmail.com>
2024-09-08 17:23:26 -0300

    Adição de view e controller de Seleção do Projeto

---
+++ primus/src/main/java/br/com/primus/primus/SelecaoDeProjetoController.java
@@ -0,0 +1,43 @@
+package br.com.primus.primus;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class SelecaoDeProjetoController{
+
+    @FXML
+    private Button botaoEntrar;
+
+    @FXML
+    private ImageView iconeProjeto1;
+
+    @FXML
+    private ImageView iconeProjeto2;
+
+    @FXML
+    private ImageView iconeProjeto3;
+
+    @FXML
+    private ImageView iconeProjeto4;
+
+    @FXML
+    private Label nomeDoProjeto1;
+
+    @FXML
+    private Label nomeDoProjeto2;
+
+    @FXML
+    private Label nomeDoProjeto3;
+
+    @FXML
+    private Label nomeDoProjeto4;
+
+    @FXML
+    void entrarNoProjeto(ActionEvent event) {
+        
+    }
+
+}
