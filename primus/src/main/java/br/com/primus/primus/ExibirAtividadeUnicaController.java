cef1245dfd139f5ab3b6fecdc658546dea221666
Diego Amorim - Dockzn <diego91077@gmail.com>
2024-09-08 18:22:31 -0300

    Adição de view e controller de exibir atividade unica

---
+++ primus/src/main/java/br/com/primus/primus/ExibirAtividadeUnicaController.java
@@ -0,0 +1,40 @@
+package br.com.primus.primus;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class ExibirAtividadeUnicaController {
+
+    @FXML
+    private Button botaoConcluirAtividade;
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    private Text complexidadeAtividade;
+
+    @FXML
+    private Text horasEstimadas;
+
+    @FXML
+    private Text localProjeto;
+
+    @FXML
+    private Text nomeAtividade;
+
+    @FXML
+    private Text responsavelAtividade;
+
+    @FXML
+    void botaoVoltarParaLista(ActionEvent event) {
+
+    }
+
+    @FXML
+    void concluirAtividade(ActionEvent event) {
+
+    }
+
+}
