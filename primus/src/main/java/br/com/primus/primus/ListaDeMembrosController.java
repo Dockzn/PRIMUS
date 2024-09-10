d95de19bc2e75c360e48108be4415bf26baff4d1
Diego Amorim - Dockzn <diego91077@gmail.com>
2024-09-09 22:32:00 -0300

    Adição de view e controller da tela Lista de Membros

---
+++ primus/src/main/java/br/com/primus/primus/ListaDeMembrosController.java
@@ -0,0 +1,67 @@
+package br.com.primus.primus;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class ListaDeMembrosController{
+
+    @FXML
+    private Label alunoMembro2;
+
+    @FXML
+    private Label alunoMembro4;
+
+    @FXML
+    private Button botaoVoltar;
+
+    @FXML
+    private Label matriculMembro7;
+
+    @FXML
+    private Label matriculaMembro1;
+
+    @FXML
+    private Label matriculaMembro3;
+
+    @FXML
+    private Label matriculaMembro5;
+
+    @FXML
+    private Label matriculaMembro6;
+
+    @FXML
+    private Label matriculaMembro8;
+
+    @FXML
+    private Label nomeMembro1;
+
+    @FXML
+    private Label nomeMembro2;
+
+    @FXML
+    private Label nomeMembro3;
+
+    @FXML
+    private Label nomeMembro4;
+
+    @FXML
+    private Label nomeMembro5;
+
+    @FXML
+    private Label nomeMembro6;
+
+    @FXML
+    private Label nomeMembro7;
+
+    @FXML
+    private HBox nomeMembro8;
+
+    @FXML
+    void voltarParaMenuDoProjeto(ActionEvent event) {
+
+    }
+
+}
